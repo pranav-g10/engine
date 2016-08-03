3213f7574170598b790aa029ad8a20b0c7ba3ab4
sweetymehta <sweety.mehta@iappstreet.com>
2016-08-03 11:36:19 +0530

    package activation added

--- spec/dummy/app/controllers/packages_controller.rb
+++ spec/dummy/app/controllers/packages_controller.rb
@@ -1,12 +1,14 @@
 class PackagesController < Locomotive::BaseController
 
-  # account_required
-  #
-  # within_site_only_if_existing true # Note: do not validate the membership
-
   layout 'layouts/application'
 
   def activate
+    @account = Locomotive::Account.find params[:id]
+    @account_pack = AccountPackage.new(account_id: @account.id)
+    if @account_pack.save
+      flash[:notice] = "Plan Activated"
+      redirect_to locomotive.sites_path
+    end
   end
 
 end
